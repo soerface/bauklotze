6ec9d37678f1ce76ccb501523121199222ffd4f6
Soeren Wegener <wegener92@gmail.com>
2015-06-08 01:18:37 +0200

    Ported Test.java to BigInteger

--- src/Test.java
+++ src/Test.java
@@ -2,139 +2,114 @@ import java.math.BigInteger;
 
 public class Test {
 
-    static long[][] example_values = {
-            {3, 1, 1},
-            {3, 2, 3},
-            {3, 3, 10},
-            {3, 4, 23},
-            {3, 5, 62},
-            {6, 1, 1},
-            {6, 2, 11},
-            {6, 3, 170},
-            {6, 4, 939},
-            {6, 5, 8342},
-            {6, 6, 80092},
-            {6, 7, 614581},
-            {6, 8, 5271923},
-            {9, 1, 1},
-            {9, 2, 41},
-            {9, 3, 3127},
-            {9, 4, 41813},
-            {9, 5, 1269900},
-            {9, 6, 45832761},
-            {9, 7, 1064557805},
-            {9, 8, 30860212081L},
-            {9, 9, 928789262080L},
-            {9, 10, 25020222581494L},
-            {9, 11, 714819627084057L},
-            {12, 1, 1},
-            {12, 2, 153},
-            {12, 3, 58234},
-            {12, 4, 1895145},
-            {12, 5, 198253934},
-            {12, 6, 27438555522L},
-            {12, 7, 1949314526229L},
-            {12, 8, 193553900967497L},
-            {12, 9, 20574308184277971L},
-            {12, 10, 1830607857363940042L},
-            // TODO: Overflow! A long is not enough
-            // {12, 11, -5675187654353494697},
-//            {12, 12, 0},
-//            {12, 13, 0},
-//            {12, 14, 0},
-            {15, 1, 1},
-            {15, 2, 571},
-            {15, 3, 1086567},
-            {15, 4, 86208957},
-            {15, 5, 31111319376L},
-            {15, 6, 16593169804557L},
-            {15, 7, 3619365754064658L},
-            {15, 8, 1235348565576072999L},
-//            {15, 9, new BigInteger("466431115279461257920")},
-            // TODO: Overflow! A long is not enough
-            // {15, 10, -9210086974413531734},
-            {18, 1, 1},
-            {18, 2, 2131},
-            {18, 3, 20279829},
-            {18, 4, 3924499731L},
-            {18, 5, 4887323351972L},
-            {18, 6, 10056816580083721L},
-            {18, 7, 6743148875847013949L},
-            // TODO: 18 8 is less than 18 7. Probably some overflow issue
-//            {18, 8, 4962389223879298519L},
-            {21, 1, 1},
-            {21, 2, 7953},
-            {21, 3, 378522507},
-            {21, 4, 178682349823L},
-            {21, 5, 767919868804309L},
-            {21, 6, 6098207777286812381L},
-            // TODO: Overflow! A long is not enough
-            // {21, 7, -7282668717286566138},
-            {24, 1, 1},
-            {24, 2, 29681},
-            {24, 3, 7065162260L},
-            {24, 4, 8135650498647L},
-            {24, 5, 120664440361104580L},
-//            {24, 6, new BigInteger("3698195100855040716832")},
-            // TODO: 24 7 is less than 24 6. Probably some overflow issue
-            // {24, 7, 3858125783825068169L},
-            {27, 1, 1},
-            {27, 2, 110771},
-            {27, 3, 131872134232L},
-            {27, 4, 370429531112741L},
-//            {27, 5, new BigInteger("18960353496615710993")},
-            // TODO: Overflow! A long is not enough
-            // {27, 6, -8218920711443338650},
-            {30, 1, 1},
-            {30, 2, 413403},
-            {30, 3, 2461410223831L},
-            {30, 4, 16866286184557689L},
-            // TODO: Overflow! A long is not enough
-            // {30, 5, -9072078150243050630},
-            {33, 1, 1},
-            {33, 2, 1542841},
-            {33, 3, 45942537263742L},
-            {33, 4, 767950873073579951L},
-            {36, 1, 1},
-            {36, 2, 5757961},
-            {36, 3, 857523348947977L},
-            // TODO: Overflow! A long is not enough
-            // {36, 4, -1927368916977437637},
-            {39, 1, 1},
-            {39, 2, 0},
-            {39, 3, 16005783272212985L},
-            {42, 1, 1},
-            {42, 2, 80198051},
-            {42, 3, 298749997296405057L},
-            {45, 1, 1},
-            {45, 2, 299303201},
-            {45, 3, 5576207010172198758L},
-            {48, 1, 1},
-            {48, 2, 1117014753},
-            // TODO: Overflow! A long is not enough
-            // {48, 3, -6599845524267142614},
-            {51, 1, 1},
-            {51, 2, 4168755811L},
-            {54, 2, 15558008491L},
-            {57, 2, 58063278153L},
-            {60, 2, 216695104121L},
-            {63, 2, 808717138331L},
-            {66, 2, 3018173449203L},
-            {69, 2, 11263976658481L},
-            {72, 2, 42037733184721L},
-            {75, 2, 156886956080403L},
-            {78, 2, 585510091136891L},
-            {81, 2, 2185153408467161L},
-            {84, 2, 8155103542731753L},
-            {87, 2, 30435260762459851L},
-            {90, 2, 113585939507107651L},
-            {93, 2, 423908497265970753L},
-            {96, 2, 1582048049556775361L},
-            {99, 2, 5904283700961130691L},
-//            {102, 2, new BigInteger("22035086754287747403"},
-//            {105, 2, },
-            // TODO: Overflow! A long is not enough
-            // {108, 2, -6685482742590689191L},
+    static BigInteger[][] example_values = {
+            {BigInteger.valueOf(3), BigInteger.valueOf(1), BigInteger.valueOf(1)},
+            {BigInteger.valueOf(3), BigInteger.valueOf(2), BigInteger.valueOf(3)},
+            {BigInteger.valueOf(3), BigInteger.valueOf(3), BigInteger.valueOf(10)},
+            {BigInteger.valueOf(3), BigInteger.valueOf(4), BigInteger.valueOf(23)},
+            {BigInteger.valueOf(3), BigInteger.valueOf(5), BigInteger.valueOf(62)},
+            {BigInteger.valueOf(6), BigInteger.valueOf(1), BigInteger.valueOf(1)},
+            {BigInteger.valueOf(6), BigInteger.valueOf(2), BigInteger.valueOf(11)},
+            {BigInteger.valueOf(6), BigInteger.valueOf(3), BigInteger.valueOf(170)},
+            {BigInteger.valueOf(6), BigInteger.valueOf(4), BigInteger.valueOf(939)},
+            {BigInteger.valueOf(6), BigInteger.valueOf(5), BigInteger.valueOf(8342)},
+            {BigInteger.valueOf(6), BigInteger.valueOf(6), BigInteger.valueOf(80092)},
+            {BigInteger.valueOf(6), BigInteger.valueOf(7), BigInteger.valueOf(614581)},
+            {BigInteger.valueOf(6), BigInteger.valueOf(8), BigInteger.valueOf(5271923)},
+            {BigInteger.valueOf(9), BigInteger.valueOf(1), BigInteger.valueOf(1)},
+            {BigInteger.valueOf(9), BigInteger.valueOf(2), BigInteger.valueOf(41)},
+            {BigInteger.valueOf(9), BigInteger.valueOf(3), BigInteger.valueOf(3127)},
+            {BigInteger.valueOf(9), BigInteger.valueOf(4), BigInteger.valueOf(41813)},
+            {BigInteger.valueOf(9), BigInteger.valueOf(5), BigInteger.valueOf(1269900)},
+            {BigInteger.valueOf(9), BigInteger.valueOf(6), BigInteger.valueOf(45832761)},
+            {BigInteger.valueOf(9), BigInteger.valueOf(7), BigInteger.valueOf(1064557805)},
+            {BigInteger.valueOf(9), BigInteger.valueOf(8), BigInteger.valueOf(30860212081L)},
+            {BigInteger.valueOf(9), BigInteger.valueOf(9), BigInteger.valueOf(928789262080L)},
+            {BigInteger.valueOf(9), BigInteger.valueOf(10), BigInteger.valueOf(25020222581494L)},
+            {BigInteger.valueOf(9), BigInteger.valueOf(11), BigInteger.valueOf(714819627084057L)},
+            {BigInteger.valueOf(12), BigInteger.valueOf(1), BigInteger.valueOf(1)},
+            {BigInteger.valueOf(12), BigInteger.valueOf(2), BigInteger.valueOf(153)},
+            {BigInteger.valueOf(12), BigInteger.valueOf(3), BigInteger.valueOf(58234)},
+            {BigInteger.valueOf(12), BigInteger.valueOf(4), BigInteger.valueOf(1895145)},
+            {BigInteger.valueOf(12), BigInteger.valueOf(5), BigInteger.valueOf(198253934)},
+            {BigInteger.valueOf(12), BigInteger.valueOf(6), BigInteger.valueOf(27438555522L)},
+            {BigInteger.valueOf(12), BigInteger.valueOf(7), BigInteger.valueOf(1949314526229L)},
+            {BigInteger.valueOf(12), BigInteger.valueOf(8), BigInteger.valueOf(193553900967497L)},
+            {BigInteger.valueOf(12), BigInteger.valueOf(9), BigInteger.valueOf(20574308184277971L)},
+            {BigInteger.valueOf(12), BigInteger.valueOf(10), BigInteger.valueOf(1830607857363940042L)},
+            {BigInteger.valueOf(15), BigInteger.valueOf(1), BigInteger.valueOf(1)},
+            {BigInteger.valueOf(15), BigInteger.valueOf(2), BigInteger.valueOf(571)},
+            {BigInteger.valueOf(15), BigInteger.valueOf(3), BigInteger.valueOf(1086567)},
+            {BigInteger.valueOf(15), BigInteger.valueOf(4), BigInteger.valueOf(86208957)},
+            {BigInteger.valueOf(15), BigInteger.valueOf(5), BigInteger.valueOf(31111319376L)},
+            {BigInteger.valueOf(15), BigInteger.valueOf(6), BigInteger.valueOf(16593169804557L)},
+            {BigInteger.valueOf(15), BigInteger.valueOf(7), BigInteger.valueOf(3619365754064658L)},
+            {BigInteger.valueOf(15), BigInteger.valueOf(8), BigInteger.valueOf(1235348565576072999L)},
+            {BigInteger.valueOf(15), BigInteger.valueOf(9), new BigInteger("466431115279461257920")},
+            {BigInteger.valueOf(18), BigInteger.valueOf(1), BigInteger.valueOf(1)},
+            {BigInteger.valueOf(18), BigInteger.valueOf(2), BigInteger.valueOf(2131)},
+            {BigInteger.valueOf(18), BigInteger.valueOf(3), BigInteger.valueOf(20279829)},
+            {BigInteger.valueOf(18), BigInteger.valueOf(4), BigInteger.valueOf(3924499731L)},
+            {BigInteger.valueOf(18), BigInteger.valueOf(5), BigInteger.valueOf(4887323351972L)},
+            {BigInteger.valueOf(18), BigInteger.valueOf(6), BigInteger.valueOf(10056816580083721L)},
+            {BigInteger.valueOf(18), BigInteger.valueOf(7), BigInteger.valueOf(6743148875847013949L)},
+            {BigInteger.valueOf(21), BigInteger.valueOf(1), BigInteger.valueOf(1)},
+            {BigInteger.valueOf(21), BigInteger.valueOf(2), BigInteger.valueOf(7953)},
+            {BigInteger.valueOf(21), BigInteger.valueOf(3), BigInteger.valueOf(378522507)},
+            {BigInteger.valueOf(21), BigInteger.valueOf(4), BigInteger.valueOf(178682349823L)},
+            {BigInteger.valueOf(21), BigInteger.valueOf(5), BigInteger.valueOf(767919868804309L)},
+            {BigInteger.valueOf(21), BigInteger.valueOf(6), BigInteger.valueOf(6098207777286812381L)},
+            {BigInteger.valueOf(24), BigInteger.valueOf(1), BigInteger.valueOf(1)},
+            {BigInteger.valueOf(24), BigInteger.valueOf(2), BigInteger.valueOf(29681)},
+            {BigInteger.valueOf(24), BigInteger.valueOf(3), BigInteger.valueOf(7065162260L)},
+            {BigInteger.valueOf(24), BigInteger.valueOf(4), BigInteger.valueOf(8135650498647L)},
+            {BigInteger.valueOf(24), BigInteger.valueOf(5), BigInteger.valueOf(120664440361104580L)},
+            {BigInteger.valueOf(24), BigInteger.valueOf(6), new BigInteger("3698195100855040716832")},
+            {BigInteger.valueOf(27), BigInteger.valueOf(1), BigInteger.valueOf(1)},
+            {BigInteger.valueOf(27), BigInteger.valueOf(2), BigInteger.valueOf(110771)},
+            {BigInteger.valueOf(27), BigInteger.valueOf(3), BigInteger.valueOf(131872134232L)},
+            {BigInteger.valueOf(27), BigInteger.valueOf(4), BigInteger.valueOf(370429531112741L)},
+            {BigInteger.valueOf(27), BigInteger.valueOf(5), new BigInteger("18960353496615710993")},
+            {BigInteger.valueOf(30), BigInteger.valueOf(1), BigInteger.valueOf(1)},
+            {BigInteger.valueOf(30), BigInteger.valueOf(2), BigInteger.valueOf(413403)},
+            {BigInteger.valueOf(30), BigInteger.valueOf(3), BigInteger.valueOf(2461410223831L)},
+            {BigInteger.valueOf(30), BigInteger.valueOf(4), BigInteger.valueOf(16866286184557689L)},
+            {BigInteger.valueOf(33), BigInteger.valueOf(1), BigInteger.valueOf(1)},
+            {BigInteger.valueOf(33), BigInteger.valueOf(2), BigInteger.valueOf(1542841)},
+            {BigInteger.valueOf(33), BigInteger.valueOf(3), BigInteger.valueOf(45942537263742L)},
+            {BigInteger.valueOf(33), BigInteger.valueOf(4), BigInteger.valueOf(767950873073579951L)},
+            {BigInteger.valueOf(36), BigInteger.valueOf(1), BigInteger.valueOf(1)},
+            {BigInteger.valueOf(36), BigInteger.valueOf(2), BigInteger.valueOf(5757961)},
+            {BigInteger.valueOf(36), BigInteger.valueOf(3), BigInteger.valueOf(857523348947977L)},
+            {BigInteger.valueOf(39), BigInteger.valueOf(1), BigInteger.valueOf(1)},
+            {BigInteger.valueOf(39), BigInteger.valueOf(2), BigInteger.valueOf(0)},
+            {BigInteger.valueOf(39), BigInteger.valueOf(3), BigInteger.valueOf(16005783272212985L)},
+            {BigInteger.valueOf(42), BigInteger.valueOf(1), BigInteger.valueOf(1)},
+            {BigInteger.valueOf(42), BigInteger.valueOf(2), BigInteger.valueOf(80198051)},
+            {BigInteger.valueOf(42), BigInteger.valueOf(3), BigInteger.valueOf(298749997296405057L)},
+            {BigInteger.valueOf(45), BigInteger.valueOf(1), BigInteger.valueOf(1)},
+            {BigInteger.valueOf(45), BigInteger.valueOf(2), BigInteger.valueOf(299303201)},
+            {BigInteger.valueOf(45), BigInteger.valueOf(3), BigInteger.valueOf(5576207010172198758L)},
+            {BigInteger.valueOf(48), BigInteger.valueOf(1), BigInteger.valueOf(1)},
+            {BigInteger.valueOf(48), BigInteger.valueOf(2), BigInteger.valueOf(1117014753)},
+            {BigInteger.valueOf(51), BigInteger.valueOf(1), BigInteger.valueOf(1)},
+            {BigInteger.valueOf(51), BigInteger.valueOf(2), BigInteger.valueOf(4168755811L)},
+            {BigInteger.valueOf(54), BigInteger.valueOf(2), BigInteger.valueOf(15558008491L)},
+            {BigInteger.valueOf(57), BigInteger.valueOf(2), BigInteger.valueOf(58063278153L)},
+            {BigInteger.valueOf(60), BigInteger.valueOf(2), BigInteger.valueOf(216695104121L)},
+            {BigInteger.valueOf(63), BigInteger.valueOf(2), BigInteger.valueOf(808717138331L)},
+            {BigInteger.valueOf(66), BigInteger.valueOf(2), BigInteger.valueOf(3018173449203L)},
+            {BigInteger.valueOf(69), BigInteger.valueOf(2), BigInteger.valueOf(11263976658481L)},
+            {BigInteger.valueOf(72), BigInteger.valueOf(2), BigInteger.valueOf(42037733184721L)},
+            {BigInteger.valueOf(75), BigInteger.valueOf(2), BigInteger.valueOf(156886956080403L)},
+            {BigInteger.valueOf(78), BigInteger.valueOf(2), BigInteger.valueOf(585510091136891L)},
+            {BigInteger.valueOf(81), BigInteger.valueOf(2), BigInteger.valueOf(2185153408467161L)},
+            {BigInteger.valueOf(84), BigInteger.valueOf(2), BigInteger.valueOf(8155103542731753L)},
+            {BigInteger.valueOf(87), BigInteger.valueOf(2), BigInteger.valueOf(30435260762459851L)},
+            {BigInteger.valueOf(90), BigInteger.valueOf(2), BigInteger.valueOf(113585939507107651L)},
+            {BigInteger.valueOf(93), BigInteger.valueOf(2), BigInteger.valueOf(423908497265970753L)},
+            {BigInteger.valueOf(96), BigInteger.valueOf(2), BigInteger.valueOf(1582048049556775361L)},
+            {BigInteger.valueOf(99), BigInteger.valueOf(2), BigInteger.valueOf(5904283700961130691L)},
     };
 
     public static void main(String[] args) {
@@ -142,28 +117,30 @@ public class Test {
         long stop;
         long delta;
         if (args.length == 2) {
-            Test.example_values = new long[1][3];
-            Test.example_values[0][0] = Long.valueOf(args[0]);
-            Test.example_values[0][1] = Long.valueOf(args[1]);
-            Test.example_values[0][2] = 0;
+            Test.example_values = new BigInteger[1][3];
+            Test.example_values[0][0] = new BigInteger(args[0]);
+            Test.example_values[0][1] = new BigInteger(args[1]);
+            Test.example_values[0][2] = BigInteger.ZERO;
         }
         for (int i = 0; i < Test.example_values.length; i++) {
-            long[] values = Test.example_values[i];
+            BigInteger[] values = Test.example_values[i];
             start = System.currentTimeMillis();
-            BigInteger res = Tetris.solve((int) values[0], (int) values[1]);
+            int m = values[0].intValue();
+            int n = values[1].intValue();
+            BigInteger res = Tetris.solve(m, n);
             stop = System.currentTimeMillis();
             delta = stop - start;
-            if (values[2] == 0 || res.compareTo(BigInteger.valueOf(values[2])) == 0) {
+            if (values[2].compareTo(BigInteger.ZERO) == 0 || res.compareTo(values[2]) == 0) {
 //                System.out.format("OK %6dms mutations: %15d setBlock: %15d setCache: %15d\n", delta, res, Tetris.setBlocks, Tetris.getCaches);
                 if (delta > 200 || true) {
-                    System.out.format("%3d %3d - OK %6dms mutations: %20d\n", values[0], values[1], delta, res);
+                    System.out.format("%3d %3d - OK %6dms mutations: %25d\n", values[0], values[1], delta, res);
                 }
             } else {
                 System.out.format("%3d %3d - ERROR\n", values[0], values[1]);
                 System.out.format("Expected %d, got %d\n", values[2], res);
                 return;
             }
-            if (i > 1 && i < Test.example_values.length - 1 && Test.example_values[i + 1][0] != values[0] && Test.example_values[i - 1][0] == values[0]) {
+            if (i > 1 && i < Test.example_values.length - 1 && Test.example_values[i + 1][0].compareTo(values[0]) != 0 && Test.example_values[i - 1][0].compareTo(values[0]) == 0) {
                 System.out.println();
             }
         }
