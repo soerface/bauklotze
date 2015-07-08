32f2532efbaa196946bb7903c0d44da781ec70cd
ojpaper <lukasried@googlemail.com>
2015-07-08 21:22:32 +0200

    arrgghh... damn this thing for not working.

--- src/Tetris.java
+++ src/Tetris.java
@@ -12,6 +12,6 @@ public class Tetris {
     }
 
     public static BigInteger solve(int m, int n) {
-        return (m > 8 || n > 8) ? TetrisS.solve(m, n) : TetrisL.solve(m, n);
+        return (m > 10 || n > 10) ? TetrisS.solve(m, n) : TetrisL.solve(m, n);
     }
 }
